b3878230b8c41e590ac7e7cc3c73ec88b3648fcb
Nicole Gilbert <nicolegilbert16@gmail.com>
2020-05-05 16:06:36 -0700

    async

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, NgZone } from '@angular/core';
 import { BLE } from '@ionic-native/ble/ngx';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'home',
@@ -9,8 +10,9 @@ import { BLE } from '@ionic-native/ble/ngx';
 export class Home {
 
   devices: any[] = [];
+  statusMessage: string;
 
-  constructor(private ble: BLE, private ngZone: NgZone) {}
+  constructor(private ble: BLE, private ngZone: NgZone, private toastCtrl: ToastController) {}
 
   ngOnInit() {
     // this.devices = [
@@ -21,9 +23,12 @@ export class Home {
   public scanForBLE() {
     console.log('scan for ble');
     this.devices = [];
-    this.ble.scan([], 15).subscribe(device => {
+    this.ble.scan([], 5).subscribe(device => {
       device => this.onDiscovered(device);
+      error => this.scanError(error)
     })
+
+    setTimeout(this.setStatus.bind(this), 5000, 'Scan complete');
   }
 
   private onDiscovered(device) {
@@ -32,5 +37,23 @@ export class Home {
       this.devices.push(device);
       console.log(device);
     })
+  }
+  
+  // If location permission is denied, you'll end up here
+  async scanError(error) {
+    this.setStatus('Error ' + error);
+    let toast = await this.toastCtrl.create({
+      message: 'Error scanning for Bluetooth low energy devices',
+      position: 'middle',
+      duration: 5000
+    });
+    toast.present();
+  }
+
+  setStatus(message) {
+    console.log(message);
+    this.ngZone.run(() => {
+      this.statusMessage = message;
+    });
   }
 }
